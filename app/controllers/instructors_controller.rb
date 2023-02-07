02e17a896b71fb2d1fb80301ecd6b223f71cbe5b
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2023-02-07 14:53:14 -0500

    added jwt tokens for user auth

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,4 +1,10 @@
 class InstructorsController < ApplicationController
+    APP_SECRET = 'secretaperature'
+    before_action :authenticate_instructor, only: [:me]
+
+    def me 
+        render json: {user: @current_instructor, user_type: "instructor"}, status: 200
+    end
 
     def index 
         render json: Instructor.all
@@ -8,7 +14,8 @@ class InstructorsController < ApplicationController
         if request.post?
             user = Instructor.find_by(instructor_code: params[:instructor_code], email: params[:email])
             if user && user.authenticate(params[:password])
-                render json: user, status: :ok
+                token = JWT.encode({user_id: user.id}, APP_SECRET, 'HS256')
+                render json: {user: user, token: token, user_type: "instructor"}, status: :ok
             else
                 render json: { error: 'Invalid credentials' }, status: :unauthorized
             end
@@ -26,4 +33,23 @@ class InstructorsController < ApplicationController
             render json: students.errors.full_messages, status: 422
         end
     end
+
+    def create
+        instructor = Instructor.create(instructor_params)
+        if instructor.valid?
+            render json: instructor, status: :created
+        else
+            render json: instructor.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    # def instructor_params
+    #     params.permit(:instructor_code, :email, :password, :site, :username).tap do |whitelisted|
+    #     whitelisted[:username] = nil 
+    #     if whitelisted[:username].blank?
+    #     end
+    # end
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+    APP_SECRET = 'secretaperature'
+    before_action :authenticate_user, only: [:me]
+
+    def me 
+        render json: {user: @current_user}, status: 200
+    end
 
     def index
         render json: User.all
@@ -6,14 +12,19 @@ class UsersController < ApplicationController
     
     def login
         if request.post?
-            user = User.find_by(parent_code: params[:parent_code], email: params[:email], password: params[:password])
-            if user
-                render json: user, status: 201
+            user = User.find_by(parent_code: params[:parent_code], email: params[:email])
+            if user && user.authenticate(params[:password])
+                token = JWT.encode({user_id: user.id}, APP_SECRET, 'HS256')
+                render json: {user: user, token: token}, status: 201
             else
                 render json: { error: 'Invalid credentials or User not found' }, status: :unauthorized
             end
         else
             render json: { error: 'Invalid request method' }, status: :bad_request
         end
+    end
+
+    def create
+
     end
 end
